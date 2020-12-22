87edb4c84abc745afe02a193308b7a38eb5d7e4e
mvskiren <kiranmvs07@gmail.com>
2020-12-22 18:36:31 +0530

    Live list nodes vs Static list nodes

--- Bank_App/script.js
+++ Bank_App/script.js
@@ -5,50 +5,12 @@ console.log(document.documentElement);
 document.querySelector('.header');
 const allSelections = document.querySelectorAll('.section');
 
+console.log(allSelections);
+
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 
 console.log(allButtons);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.btn--close-modal');
-// const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-// const openModal = function () {
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
+//Difference between querySelector vs getElementById 
+//live Node List(getElementByTagName) Vs Static List
